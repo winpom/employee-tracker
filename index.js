976c8c33aea05043497629de6658a3b15ff1bd2f
Tyrion Lannister <win.pomerantz@gmail.com>
2024-04-26 12:57:00 -0700

    added user questions

---
+++ index.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+const inquirer = require("inquirer");
+const { Pool } = require('pg');
+
+// Questions for user
+const questions = [{
+    type: "list",
+    message: "What would you like to do? (Use arrow keys)",
+    name: "open",
+    choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"], 
+},
+{
+    when: input => {
+        return input.open == "Add Department"
+    },
+    type: "input",
+    message: "What is the name of the department?",
+    name: "departmentName",
+},
+{
+    when: input => {
+        return input.open == "Add Role"
+    },
+    type: "input",
+    message: "What is the name of the role?",
+    name: "roleName",
+},
+{
+    when: input => {
+        return input.roleName == true
+    },
+    type: "input",
+    message: "What is the salary of the role?",
+    name: "roleSalary",
+},
+{
+    when: input => {
+        return input.roleSalary == true
+    },
+    type: "list",
+    message: "What department does the role belong to? (Use arrow keys)",
+    name: "roleDept",
+    choices: [],
+},
+{
+    when: input => {
+        return input.open == "Add Employee"
+    },
+    type: "input",
+    message: "What is the employee's first name?",
+    name: "employeeFirstName",
+},
+{
+    when: input => {
+        return input.employeeFirstName == true
+    },
+    type: "input",
+    message: "What is the employee's last name?",
+    name: "employeeLastName",
+},
+{
+    when: input => {
+        return input.employeeLastName == true
+    },
+    type: "list",
+    message: "What is the employee's role? (Use arrow keys)",
+    name: "empRole",
+    choices: [],
+},
+{
+    when: input => {
+        return input.empRole == true
+    },
+    type: "list",
+    message: "Who is the employee's manager? (Use arrow keys)",
+    name: "empManager",
+    choices: [],
+},
+{
+    when: input => {
+        return input.open == "Update Employee Role"
+    },
+    type: "list",
+    message: "Which employee's role would you like to update? (Use arrow keys)",
+    name: "empRoleUpdate",
+    choices: [],
+},
+{
+    when: input => {
+        return input.empRoleUpdate == true
+    },
+    type: "list",
+    message: "Which role would you like to assign to the selected employee? (Use arrow keys)",
+    name: "assignRole",
+    choices: [],
+},]
